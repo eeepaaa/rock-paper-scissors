b7f2324985d326ba9396dd3b5554eebadb656e90
Cecil Thomas <cejohanner@gmail.com>
2023-08-11 13:40:40 -0400

    Finish game()
    However, it does not validate user's input.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let picksAvailable = ['Rock', 'Paper', 'Scissors'];
 let getRandomPicksIndex = () => Math.floor((Math.random() * 3));
 let getComputerChoice = () => picksAvailable[getRandomPicksIndex()];
-let playerScore = 0;
-let computerScore = 0;
+let scores = {player: 0, computer: 0}
+let canIncrease = false
 
 function capitalizeString(string) {  
     let getFirstLetter = string.charAt(0);
@@ -11,22 +11,45 @@ function capitalizeString(string) {
     return upperCaseFirstLetter + getRemainingLetters
 }
 
+function addPointToScore(player) {
+    if (canIncrease) {
+        scores[`${player}`] += 1;
+    }
+}
+
 function playRound(playerSelection) {
     let computerSelection = getComputerChoice();
 
     if ((playerSelection == 'Rock' && computerSelection == 'Scissors') ||
         (playerSelection == 'Scissors' && computerSelection == 'Paper') ||
         (playerSelection == 'Paper' && computerSelection == 'Rock')) {
-        // declare setter and getter methods
-        playerScore += 1;
-        // 
+        addPointToScore('player');
         return `You win! ${playerSelection} beats ${computerSelection}.`
     } else if (playerSelection == computerSelection) {
         return 'It\'s a tie!';
     } else {
-        // declare setter and getter methods
-        computerScore += 1;
-        //
+        addPointToScore('computer');
         return `You lose! ${computerSelection} beats ${playerSelection}.`;
     }
+}
+
+function game() {
+    canIncrease = true
+    let playerSelection;
+
+    for (let index = 0; index < 5; index++) {
+        playerSelection = capitalizeString(prompt('Rock, Paper or Scissors?').toLowerCase().trim())
+        console.log(playRound(playerSelection))
+    }
+
+    if (scores['player'] > scores['computer']) {
+        console.log(`You win ${scores['player']} to ${scores['computer']}.`)
+    } else if (scores['player'] < scores['computer']) {
+        console.log(`You lose ${scores['player']} to ${scores['computer']}.`)
+    } else {
+        console.log(`You tied ${scores['player']} to ${scores['computer']}.`)
+    }
+
+    scores = {player: 0, computer: 0}
+    canIncrease = false
 }
