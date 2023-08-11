c68140a98d571c1aaf671bbf9bab978392bf6a22
Cecil Thomas <cejohanner@gmail.com>
2023-08-11 09:35:52 -0400

    Finish capitalizeString()

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let picksAvailable = ['rock', 'paper', 'scissors'];
+let picksAvailable = ['Rock', 'Paper', 'Scissors'];
 let getRandomPicksIndex = () => Math.floor((Math.random() * 3));
 let getComputerChoice = () => picksAvailable[getRandomPicksIndex()];
 
+function capitalizeString(string) {  
+    let getFirstLetter = string.charAt(0);
+    let getRemainingLetters = string.slice(1);
+    let upperCaseFirstLetter = getFirstLetter.toUpperCase();
+    return upperCaseFirstLetter + getRemainingLetters
+}
