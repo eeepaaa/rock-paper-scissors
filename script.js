82ca58ac3600601408b73e82b14e716681e2127d
Cecil Thomas <cejohanner@gmail.com>
2023-08-11 09:52:42 -0400

    Finish playRound()

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 let picksAvailable = ['Rock', 'Paper', 'Scissors'];
 let getRandomPicksIndex = () => Math.floor((Math.random() * 3));
 let getComputerChoice = () => picksAvailable[getRandomPicksIndex()];
+let playerScore = 0;
+let computerScore = 0;
 
 function capitalizeString(string) {  
     let getFirstLetter = string.charAt(0);
     let getRemainingLetters = string.slice(1);
     let upperCaseFirstLetter = getFirstLetter.toUpperCase();
     return upperCaseFirstLetter + getRemainingLetters
+}
+
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+
+    if ((playerSelection == 'Rock' && computerSelection == 'Scissors') ||
+        (playerSelection == 'Scissors' && computerSelection == 'Paper') ||
+        (playerSelection == 'Paper' && computerSelection == 'Rock')) {
+        // declare setter and getter methods
+        playerScore += 1;
+        // 
+        return `You win! ${playerSelection} beats ${computerSelection}.`
+    } else if (playerSelection == computerSelection) {
+        return 'It\'s a tie!';
+    } else {
+        // declare setter and getter methods
+        computerScore += 1;
+        //
+        return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    }
 }
